a634e9dfea34d59a844c80e76ed731693cc47776
Ryan <rbrickproductions123@gmail.com>
2014-09-19 16:01:18 -0500

    Start work on the SubCommand System
    ^ wat that said m8 u got a problem? u want to fite meh irl

--- src/me/rbrick/zeus/annotations/SubCommand.java
+++ src/me/rbrick/zeus/annotations/SubCommand.java
@@ -1,5 +1,10 @@
 package me.rbrick.zeus.annotations;
 
+import javax.annotation.Nonnull;
+
 public @interface SubCommand {
-    // TODO
+    
+    @Nonnull String parent() default ""; // The parent command e.g. /example would be the parent command.
+    
+    @Nonnull String name() default ""; // The actual command e.g. /example demostrate. demostrate would be the subcommand.
 }
