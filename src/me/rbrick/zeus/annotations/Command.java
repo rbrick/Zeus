6960b5521ad253a599da48dc46712fcc3bdb5cf6
rbrick <rbrickproductions123@gmail.com>
2014-10-18 17:40:30 -0500

    Some how forgot this! Thanks to @zixel for catching this! Credit goes to him :)

--- src/me/rbrick/zeus/annotations/Command.java
+++ src/me/rbrick/zeus/annotations/Command.java
@@ -1,10 +1,17 @@
 package me.rbrick.zeus.annotations;
 
 import javax.annotation.Nonnull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * This code is copyrighted by rbrick and the BreakMC Network.
  */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Command {
     @Nonnull String name() default "";
 
--- src/me/rbrick/zeus/annotations/SubCommand.java
+++ src/me/rbrick/zeus/annotations/SubCommand.java
@@ -1,7 +1,13 @@
 package me.rbrick.zeus.annotations;
 
 import javax.annotation.Nonnull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface SubCommand {
 
    @Nonnull String parent() default "";
